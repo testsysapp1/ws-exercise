bd64be7cad5ff4afcba50850e66aba5acec8941f
testsysapp1 <macbookair@Macbooks-MacBook-Air.local>
2020-11-29 16:44:39 +1300

    1st commit

--- cypress/integration/kscalculator.js
+++ cypress/integration/kscalculator.js
@@ -10,43 +10,35 @@ describe('KiwiSaver Retirement Calculator', function()
             
         cy.get('#ubermenu-section-1-6-ps').trigger('mouseover')
         cy.get('.sw-ubermenu-column-items')
-            // .get('.sw-ubermenu-column-item.Primary')
             .get('#ubermenu-item-cta-kiwisaver-calculators-ps').click({multiple: true})
         
         cy.get('.sw-sidenav-item-link.link')
             .get('.sw-sidenav-item-link-text.last:visible').click()
 
-            const getIframe = cy
-                .get('#calculator-embed') //icon-target.icon-target-help-toggle.icon-btn.icon-btn-info.ir.ng-scope')
-                .get ('iframe')
-                .its('0.contentDocument.body')
-                .should('be.visible')
-                .then(cy.wrap);
-
-            //getIframe('.icon-target.icon-target-help-toggle.icon-btn.icon-btn-info.ir.ng-scope'))
+        const iframe = cy
+            .get('#calculator-embed') 
+            .get ('iframe')
+            .its('0.contentDocument.body')
+            .should('be.visible')
+            .then(cy.wrap)
+            
+            .get('#CurrentAge').click()           
+              
     })
 
-    // it('Calculate projected balances where age = 30, employed, @82000pa, @4%, and Defensive Risk ', function() 
-    // {
-    //     cy.get('.wpnib-field-current-age.field-group.ng-isolate-scope')
-    //         // .get('.text').type(config.tc[0].age)
+    it('Calculate projected balances where age = 30, employed, @82000pa, @4%, and Defensive Risk ', function() 
+    {   
+        //cy.get('.wpnib-field-current-age.field-group.ng-isolate-scope')
+        // .get('text').type(config.tc[0].age)
+    })
 
-    // })
+    it('Calculate projected balances where age = 45, self-employed, @100000pa, voluntary contribution 90fn, Conservative Risk, saving goal 290,000', function() 
+    {
 
-    // it('Calculate projected balances where age = 45, self-employed, @100000pa, voluntary contribution 90fn, Conservative Risk, saving goal 290,000', function() 
-    // {
-    //     cy.get('')
-    //         // .get('.text').type('45')
+    })
 
-    // })
+    it('Calculate projected balances where age = 55, not-employed, current balance 140,000, voluntary contribution 10pa, Balance Risk, saving goal 200,000', function() 
+    {
 
-    // it('Calculate projected balances where age = 55, not-employed, current balance 140,000, voluntary contribution 10pa, Balance Risk, saving goal 200,000', function() 
-    // {
-    //     cy.get('')
-    //     // .get('.text').type('45')
-
-    // })
-
-
-
+    })
 })
